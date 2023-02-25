5b90d97bdea7bb87021a91dd715b10b5eb67b60a
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-02-25 02:56:23 +0200

    selecting randow dices side is implemented

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const diceTwoSixthSide  = [...diceOneSecondSide, ...document.querySelectorAll(".
 const dice1 = document.getElementById("dice1");
 const dice2 = document.getElementById("dice2");
 const rollBtn = document.getElementById("roll");
+const circles = document.getElementsByClassName("circles");
 const rollingSound = document.createElement("audio");
 rollingSound.src = "./sounds/dice-sound-effect_HoCT2XxU.mp3";
 rollBtn.addEventListener("mousedown", rollTheDices);
@@ -39,5 +40,57 @@ function rotateTheDices(){
         dice2.style.rotate = `${n}deg`;
         n += 6;
     }, 6);
-    setTimeout(() => rollBtn.addEventListener("mousedown", rollTheDices), 1440);
+    setTimeout(() => {
+        rollBtn.addEventListener("mousedown", rollTheDices);
+        selectSideAfterRolling();
+    }, 1440);
+}
+
+function selectSideAfterRolling(){
+    Array.from(circles).forEach((elem) => elem.style.visibility = "hidden");
+    const diceOneSide = Math.floor(Math.random() * 6) + 1;
+    const diceTwoSide = Math.floor(Math.random() * 6) + 1;
+    console.log(diceOneSide, diceTwoSide);
+    checkSide(diceOneSide, diceTwoSide);
+    checkWinner(diceOneSide, diceTwoSide);
+}
+function checkSide(n1, n2){
+    switch(n1){
+        case 1:
+            diceOneFirstSide[0].style.visibility = "visible";
+            break;
+        case 2:
+            diceOneSecondSide.forEach((elem) => elem.style.visibility = "visible");
+            break;
+        case 3:
+            diceOneThirdSide.forEach((elem) => elem.style.visibility = "visible");
+            break;
+        case 4:
+            diceOneFourthSide[0].style.visibility = "visible";
+            break;
+        case 5:
+            diceOneFifthSide[0].style.visibility = "visible";
+            break;
+        case 6:
+            diceOneSixthSide.forEach((elem) => elem.style.visibility = "visible");
+    };
+    switch(n2){
+        case 1:
+            diceTwoFirstSide[0].style.visibility = "visible";
+            break;
+        case 2:
+            diceTwoSecondSide.forEach((elem) => elem.style.visibility = "visible");
+            break;
+        case 3:
+            diceTwoThirdSide.forEach((elem) => elem.style.visibility = "visible");
+            break;
+        case 4:
+            diceTwoFourthSide[0].style.visibility = "visible";
+            break;
+        case 5:
+            diceTwoFifthSide[0].style.visibility = "visible";
+            break;
+        case 6:
+            diceTwoSixthSide.forEach((elem) => elem.style.visibility = "visible");
+    }
 }
