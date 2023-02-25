d5f91334c13a668f3da0094ae9b21277167e3fea
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-02-25 03:21:07 +0200

    the first version of 'Dice' game has been released

--- main.js
+++ main.js
@@ -9,12 +9,16 @@ const diceTwoSecondSide = [document.querySelector(".circles2:nth-child(4)"), doc
 const diceTwoThirdSide  = [document.querySelector(".circles2:nth-child(3)"), document.querySelector(".circles2:nth-child(5)"), document.querySelector(".circles2:nth-child(7)")];
 const diceTwoFourthSide = [document.querySelectorAll(".circles2:nth-child(2n)")];
 const diceTwoFifthSide  = [document.querySelectorAll(".circles2:nth-child(2n + 1)")];
-const diceTwoSixthSide  = [...diceOneSecondSide, ...document.querySelectorAll(".last-side")];
+const diceTwoSixthSide  = [...diceTwoSecondSide, ...document.querySelectorAll(".last-side-two")];
 const dice1 = document.getElementById("dice1");
 const dice2 = document.getElementById("dice2");
 const rollBtn = document.getElementById("roll");
 const circles = document.getElementsByClassName("circles");
 const rollingSound = document.createElement("audio");
+const welcome = document.getElementById("welcome");
+const player1 = document.getElementById("player1");
+const player2 = document.getElementById("player2");
+const draw = document.getElementById("draw");
 rollingSound.src = "./sounds/dice-sound-effect_HoCT2XxU.mp3";
 rollBtn.addEventListener("mousedown", rollTheDices);
 function rollTheDices(e){
@@ -50,7 +54,6 @@ function selectSideAfterRolling(){
     Array.from(circles).forEach((elem) => elem.style.visibility = "hidden");
     const diceOneSide = Math.floor(Math.random() * 6) + 1;
     const diceTwoSide = Math.floor(Math.random() * 6) + 1;
-    console.log(diceOneSide, diceTwoSide);
     checkSide(diceOneSide, diceTwoSide);
     checkWinner(diceOneSide, diceTwoSide);
 }
@@ -60,37 +63,53 @@ function checkSide(n1, n2){
             diceOneFirstSide[0].style.visibility = "visible";
             break;
         case 2:
-            diceOneSecondSide.forEach((elem) => elem.style.visibility = "visible");
+            diceOneSecondSide.forEach(elem => elem.style.visibility = "visible");
             break;
         case 3:
-            diceOneThirdSide.forEach((elem) => elem.style.visibility = "visible");
+            diceOneThirdSide.forEach(elem => elem.style.visibility = "visible");
             break;
         case 4:
-            diceOneFourthSide[0].style.visibility = "visible";
+            diceOneFourthSide[0].forEach(elem => elem.style.visibility = "visible");
             break;
         case 5:
-            diceOneFifthSide[0].style.visibility = "visible";
+            diceOneFifthSide[0].forEach(elem => elem.style.visibility = "visible");
             break;
         case 6:
-            diceOneSixthSide.forEach((elem) => elem.style.visibility = "visible");
+            diceOneSixthSide.forEach(elem => elem.style.visibility = "visible");
     };
     switch(n2){
         case 1:
             diceTwoFirstSide[0].style.visibility = "visible";
             break;
         case 2:
-            diceTwoSecondSide.forEach((elem) => elem.style.visibility = "visible");
+            diceTwoSecondSide.forEach(elem => elem.style.visibility = "visible");
             break;
         case 3:
-            diceTwoThirdSide.forEach((elem) => elem.style.visibility = "visible");
+            diceTwoThirdSide.forEach(elem => elem.style.visibility = "visible");
             break;
         case 4:
-            diceTwoFourthSide[0].style.visibility = "visible";
+            diceTwoFourthSide[0].forEach(elem => elem.style.visibility = "visible");
             break;
         case 5:
-            diceTwoFifthSide[0].style.visibility = "visible";
+            diceTwoFifthSide[0].forEach(elem => elem.style.visibility = "visible");
             break;
         case 6:
-            diceTwoSixthSide.forEach((elem) => elem.style.visibility = "visible");
+            diceTwoSixthSide.forEach(elem => elem.style.visibility = "visible");
+    }
+}
+function checkWinner(n1, n2){
+    welcome.style.visibility = "hidden";
+    if(n1 > n2){
+        player1.style.visibility = "visible";
+        player2.style.visibility = "hidden";
+        draw.style.visibility = "hidden";
+    }else if(n2 > n1){
+        player1.style.visibility = "hidden";
+        player2.style.visibility = "visible";
+        draw.style.visibility = "hidden";
+    }else{
+        player1.style.visibility = "hidden";
+        player2.style.visibility = "hidden";
+        draw.style.visibility = "visible";
     }
 }
